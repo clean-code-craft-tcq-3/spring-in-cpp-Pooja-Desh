373e9968ad38a69437e226e0c68d2f85744be242
Pooja-Desh <100403024+Pooja-Desh@users.noreply.github.com>
2022-03-02 23:26:59 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,28 @@
 #include "stats.h"
+using namespace Statistics;
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+Stats ComputeStatistics(const std::vector<double>& InputData) 
+{
+    double sum = 0,average,max=0,min=0;
+    for (int i=0; i<sizeof(InputData); i++)
+    {
+        sum += InputData[i];
+    }
+    average = sum/sizeof(InputData);
+
+    for (int i=0; i<sizeof(InputData); i++)
+    {
+        if(max < InputData[i])
+        {
+            max = InputData[i]; 
+        }
+
+        if(min > InputData[i])
+        {
+            min = InputData[i]; 
+        }
+    }  
+    ComputeStatistics.average = average;
+    ComputeStatistics.max = max;
+    ComputeStatistics.min = min;
 }
