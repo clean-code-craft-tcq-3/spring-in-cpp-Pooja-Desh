9127487e12c43e7d4b40b498840074ba8575f949
Pooja-Desh <100403024+Pooja-Desh@users.noreply.github.com>
2022-03-03 00:26:30 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,7 +1,7 @@
 #include "stats.h"
-using namespace Statistics;
+//using namespace Statistics;
 
-Stats Stats::ComputeStatistics(const std::vector<double>& InputData) 
+Stats Statistics::ComputeStatistics(const std::vector<double>& InputData) 
 {
     Stats ComputeStatIstics_Temp;
     double sum = 0,average,max=0,min=0;
