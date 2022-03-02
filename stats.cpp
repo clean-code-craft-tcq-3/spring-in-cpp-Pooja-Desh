4e35c286b08cbc2558f4b0add286859b1fdc0bae
Pooja-Desh <100403024+Pooja-Desh@users.noreply.github.com>
2022-03-03 00:22:20 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -5,13 +5,13 @@ Stats Stats::ComputeStatistics(const std::vector<double>& InputData)
 {
     Stats ComputeStatIstics_Temp;
     double sum = 0,average,max=0,min=0;
-    for (int i=0; i<InputData.size(); i++)
+    for (int i=0; i<int(InputData.size()); i++)
     {
         sum += InputData[i];
     }
     average = sum/sizeof(InputData);
 
-    for (int i=0; i<sizeof(InputData); i++)
+    for (int i=0; i<int(InputData.size()); i++)
     {
         if(max < InputData[i])
         {
