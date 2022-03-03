c3be29a4293e29ef44fa798e9102864a10fa4e25
Pooja-Desh <100403024+Pooja-Desh@users.noreply.github.com>
2022-03-04 02:18:28 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,19 +1,68 @@
 #include <vector>
 
-
 class Stats
-    {
-       public:
-       float average, max, min; 
+{
+public:
+    float average, max, min;
 
-       //constructor
-       //Stats(const std::vector<double>& inputdata);
-    //};
+    //constructor
+    //Stats(const std::vector<double>& inputdata);
+};
 
     //}ComputeStatIstics_Temp;
-    };
     
 namespace Statistics 
 {
     Stats ComputeStatistics(const std::vector<float>& inputData);
 }
+
+class IAlerter
+{
+    /*int alert_flag;
+    virtual void set_alert()
+    {
+        alert_flag = 0;
+    }*/
+}
+
+class EmailAlert::public IAlerter
+{
+    bool emailSent;
+    void alert_email()
+    {
+        emailSent = true;
+    }
+}
+
+class LEDAlert::public IAlerter
+{
+    bool ledGlows;
+    void alert_LED()
+    {
+        ledGlows = true;
+    }
+}
+
+class StatsAlerter::public IAlerter
+{
+    private:
+    float maxThreshold;
+    std::vector<float> inputData;
+    std::vector<IAlerter> alerters_input;
+
+    public:
+    checkAndAlert(const std::vector<float>& inputData);
+
+    //parametrised constructor
+    StatsAlerter(maxThreshold_temp, alerters)
+    {
+        maxThreshold = maxThreshold_temp;
+        alerters_input = alerters;
+    }
+
+}
+
+
+
+//alerters is vector of struct
+
